f48af7a80a172e3288951da5f6aa6e3a122fe45f
Saksham Kumar <90099687+saksham7797@users.noreply.github.com>
2025-11-28 13:53:30 +0530

    Restrict CORS to specific frontend URL

--- src/main/java/com/sakku/Mausam/Controller.java
+++ src/main/java/com/sakku/Mausam/Controller.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
 
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "https://mausam-frontend-eta.vercel.app")
 @RestController
 @Cacheable(value = "weather_cache", key = "#city")
 public class Controller {
